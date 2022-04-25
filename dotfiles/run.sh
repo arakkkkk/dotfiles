3e3245172f3d9e2a965e4f49d8d56fc343a2d9ad
kinoshitalab <kouiti@arakkk.local>
2022-04-25 12:41:12 +0900

    Jioj

---
+++ dotfiles/run.sh
@@ -0,0 +1,6 @@
+# pytest ~/localfile/glycosim/dev-glycosim/tests/func/Pyomo/test_create_model.py
+# pytest ~/localfile/glycosim/dev-glycosim/tests/func/Pyomo/test_simulate.py
+pytest ~/localfile/glycosim/dev-glycosim/tests/func/GPP/test_gpp.py
+# pytest ~/localfile/glycosim/dev-glycosim/tests/unit/test_licorr.py
+# pytest ~/localfile/glycosim/dev-glycosim/tests/func/GPP/test_main.py
+# python ~/localfile/practice/score_ipynb/score.py

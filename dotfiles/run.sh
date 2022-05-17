f0e47864aae3731c2236081257396432d42816bf
kinoshitalab <kouiti@arakkk.local>
2022-05-17 14:28:01 +0900

    jioj

--- dotfiles/run.sh
+++ dotfiles/run.sh
@@ -1,6 +1,10 @@
 # pytest ~/localfile/glycosim/dev-glycosim/tests/func/Pyomo/test_create_model.py
 # pytest ~/localfile/glycosim/dev-glycosim/tests/func/Pyomo/test_simulate.py
-pytest ~/localfile/glycosim/dev-glycosim/tests/func/GPP/test_gpp.py
-# pytest ~/localfile/glycosim/dev-glycosim/tests/unit/test_licorr.py
+
+# pytest ~/localfile/glycosim/dev-glycosim/tests/func/SBML/test_create_model.py
+pytest ~/localfile/glycosim/dev-glycosim/tests/func/SBML/test_simulate.py
+
+# pytest ~/localfile/glycosim/dev-glycosim/tests/func/GPP/test_gpp.py
 # pytest ~/localfile/glycosim/dev-glycosim/tests/func/GPP/test_main.py
 # python ~/localfile/practice/score_ipynb/score.py
+# python ~/localfile/practice/mathviwer/app.py
